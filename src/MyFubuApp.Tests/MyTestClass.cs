ad21d4559c9df11e21eef032b03fb3df5a3e7141
Jacob Meline <jacmeli@SLV-JACMELI01.extendhealth.com>
2015-06-10 12:46:18 -0600

    Initial Commit

---
+++ src/MyFubuApp.Tests/MyTestClass.cs
@@ -0,0 +1,16 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace MyFubuApp.Tests
+{
+    public class MyTestClass
+    {
+        [Fact]
+        public void MyTestMethod()
+        {
+            var test = "my test string";
+
+            test.ShouldBe("my test string");
+        }
+    }
+}
---
+++ src/MyFubuApp/MyFubuApplication.cs
@@ -0,0 +1,35 @@
+﻿using FubuMVC.Core;
+using FubuMVC.StructureMap;
+using StructureMap;
+using StructureMap.Configuration.DSL;
+
+namespace MyFubuApp
+{
+
+    public class MyFubuApplication : IApplicationSource
+    {
+
+        public FubuApplication BuildApplication()
+        {
+            var fubuApp = FubuApplication.For<MyFubuApplicationRegistry>().StructureMap();
+            return fubuApp;
+        }
+    }
+
+    public class MyFubuApplicationRegistry : FubuRegistry
+    {
+        public MyFubuApplicationRegistry()
+        {
+
+        }        
+    }
+
+    public class MyRegistry : Registry
+    {
+        public MyRegistry()
+        {
+            For<IApplicationSource>().Use<MyFubuApplication>();
+            
+        }
+    }
+}
