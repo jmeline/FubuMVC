7613080633f35946370608349e5673725c655df6
Jacob Meline <jacob.meline@gmail.com>
2016-03-14 11:56:45 -0600

    basic fubu3 app running

---
+++ src/Fubu3TodoApp/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Fubu3TodoApp.Settings;
+using FubuCore;
+using FubuMVC.Core;
+
+namespace Fubu3TodoApp
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        private FubuRuntime _runtime;
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            _runtime = FubuRuntime.For<FubuTodoRegistry>();
+        }
+
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+            if (_runtime != null)
+            {
+                _runtime.SafeDispose();
+            }
+        }
+    }
+}
---
+++ src/Fubu3TodoApp/Settings/FubuTodoRegistry.cs
@@ -0,0 +1,27 @@
+﻿using FubuMVC.Core;
+using StructureMap;
+
+namespace Fubu3TodoApp.Settings
+{
+    public class FubuTodoRegistry : FubuRegistry
+    {
+        public FubuTodoRegistry()
+        {
+            Features.Diagnostics.Enable(TraceLevel.Verbose);
+            Actions.IncludeClassesSuffixedWithEndpoint();
+
+            StructureMap(new Container( new FubuTodoStructureMapRegistry() ));
+        }
+    }
+
+    public class FubuTodoStructureMapRegistry : Registry
+    {
+        public FubuTodoStructureMapRegistry()
+        {
+            Scan(x =>
+            {
+                x.WithDefaultConventions();
+            });
+        }
+    }
+}
