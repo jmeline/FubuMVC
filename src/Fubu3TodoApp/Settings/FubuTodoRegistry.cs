6c8c16088a94b77a6611a25959004181729e21f3
Jacob Meline <jacob.meline@gmail.com>
2016-03-15 09:04:00 -0600

    Added a basic behavior when user hits endpoint

--- src/Fubu3TodoApp/Settings/FubuTodoRegistry.cs
+++ src/Fubu3TodoApp/Settings/FubuTodoRegistry.cs
@@ -1,5 +1,11 @@
-﻿using FubuMVC.Core;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using FubuMVC.Core;
+using FubuMVC.Core.Behaviors;
+using FubuMVC.Core.Registration;
+using FubuMVC.Core.Registration.Nodes;
 using StructureMap;
+using TraceLevel = FubuMVC.Core.TraceLevel;
 
 namespace Fubu3TodoApp.Settings
 {
@@ -10,10 +16,59 @@ namespace Fubu3TodoApp.Settings
             Features.Diagnostics.Enable(TraceLevel.Verbose);
             Actions.IncludeClassesSuffixedWithEndpoint();
 
-            StructureMap(new Container( new FubuTodoStructureMapRegistry() ));
+            Policies.Global.Add<SamplePolicy>();
+
+            StructureMap(new Container(new FubuTodoStructureMapRegistry()));
         }
     }
 
+    public class SamplePolicy : IConfigurationAction
+    {
+        public void Configure(BehaviorGraph graph)
+        {
+            foreach (var action in graph.Actions())
+            {
+                if (action.Method.DeclaringType != null && 
+                    action.Method.DeclaringType.Name == "HomeEndpoint")
+                {
+                    action.AddBefore(new Wrapper(typeof (SayHelloBehavior)));
+                    action.AddBefore(new Wrapper(typeof (SayGoodbyeBehavior)));
+                }
+            }
+        }
+    }
+
+    public class SayHelloBehavior : IActionBehavior
+    {
+        public IActionBehavior InsideBehavior { get; set; }
+
+        public void Invoke()
+        {
+            Debug.WriteLine("Hello");
+            InsideBehavior?.Invoke();
+        }
+
+        public void InvokePartial()
+        {
+            InsideBehavior?.InvokePartial();
+        }
+    }
+
+    public class SayGoodbyeBehavior : IActionBehavior
+    {
+        public IActionBehavior InsideBehavior { get; set; }
+
+        public void Invoke()
+        {
+            Debug.WriteLine("GoodBye");
+            InsideBehavior?.Invoke();
+        }
+
+        public void InvokePartial()
+        {
+            InsideBehavior?.InvokePartial();
+        }
+    }
     public class FubuTodoStructureMapRegistry : Registry
     {
         public FubuTodoStructureMapRegistry()
--- src/old/MyFubuApp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyFubuApp.Global" Language="C#" %>
