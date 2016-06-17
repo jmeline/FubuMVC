4c531d6934469d50417684de069a9e85647a5015
Jacob Meline <jacob.meline@gmail.com>
2016-06-17 14:48:17 -0600

    fixing navigation bar

--- src/Fubu3TodoApp/Content/js/app.jsx
+++ src/Fubu3TodoApp/Content/js/app.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Home from './containers/home.jsx';
-// import Todo from './containers/todo.jsx';
-import { Router, Route, browserHistory } from 'react-router';
+import Todo from './components/todo.jsx';
+import { Router, Route, browserHistory, IndexRoute} from 'react-router';
 
 render((
   <Router history={browserHistory}>
     <Route path="/" component={Home}>
       <IndexRoute component={Home} />
-      {/* <Route path='todo' component={Todo} /> */}
+      <Route path='todo' component={Todo} />
     </Route>
 
   </Router>), document.getElementById("app")
--- src/Fubu3TodoApp/Content/js/components/navigation.jsx
+++ src/Fubu3TodoApp/Content/js/components/navigation.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Nav from 'react-bootstrap/lib/Nav';
 import Navbar from 'react-bootstrap/lib/Navbar';
 import NavItem from 'react-bootstrap/lib/NavItem';
+import { Link } from 'react-router';
 
 export default class Navigation extends React.Component {
   constructor(props) {
@@ -11,7 +12,10 @@ export default class Navigation extends React.Component {
   render() {
     return (
       <div className="navbarTheme navbarFixedTop">
-        <a href="#" > TW-TODO </a>
+        <ul>
+          <li> <Link to="/">TW-TODO</Link> </li>
+          <li> <Link to="/todo">Todo</Link> </li>
+        </ul>
       </div>
     );
   }
