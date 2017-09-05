5620e6c480167707cc1a6f1db298645b19ad0f66
jmeline <jacob.meline@gmail.com>
2017-09-05 08:10:27 -0600

    Updating project dependencies to use the latest marten

---
+++ src/CSharp_Language/Computer Science Theory/ValueAndReferenceTypes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Shouldly;
+using Xunit;
+
+namespace CSharp_Language.Computer_Science_Theory
+{
+    public class ValueAndReferenceTypes
+    {
+        public void OfTypeStuctAndBaseTypeValueType<T>()
+        {
+            typeof(T).IsValueType.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ValueTypes()
+        {
+            OfTypeStuctAndBaseTypeValueType<double>();
+            OfTypeStuctAndBaseTypeValueType<long>();
+            OfTypeStuctAndBaseTypeValueType<short>();
+        }
+    }
+}
