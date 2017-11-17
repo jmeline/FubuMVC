be32d8c5fb5b4ca408c9503570b8d377a623f0b6
jmeline <jacob.meline@gmail.com>
2017-11-17 09:22:50 -0700

    Adding design patterns to my FubuMVC project

---
+++ src/DesignPatterns/Structural Patterns/Adapter(Wrapper)/AdapterTests.cs
@@ -0,0 +1,25 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace DesignPatterns.Structural_Patterns
+{
+    /// <summary>
+    /// Convert the interface of a class into another interface clients expect. 
+    /// Adapter lets classes work together that couldn't otherwise because of incompatible interfaces.
+    ///
+    /// Frequency of use: Medium high (4/5)
+    /// </summary>
+    public class AdapterTests
+    {
+        [Fact]
+        public void TestStructrualExample()
+        {
+            var target = new Target();
+            target.Request().ShouldBe("Target");
+            var adaptee = new Adaptee();
+            var cheeseAdaptee = new CheeseAdaptee();
+            new Adapter(adaptee).Request().ShouldBe("Adaptee");
+            new Adapter(cheeseAdaptee).Request().ShouldBe("Cheese");
+        }
+    }
+}
---
+++ src/DesignPatterns/Structural Patterns/Adapter(Wrapper)/StructuralExample.cs
@@ -0,0 +1,35 @@
+﻿
+namespace DesignPatterns.Structural_Patterns
+{
+    public interface IAdapt
+    {
+        string Request();
+    }
+
+    public class Adapter : Target
+    {
+        private readonly IAdapt _adaptee;
+
+        public Adapter(IAdapt adaptee) 
+        {
+            _adaptee = adaptee;
+        }
+
+        public override string Request() => _adaptee.Request();
+    }
+
+    public class Adaptee : IAdapt
+    {
+        public string Request() => "Adaptee";
+    }
+
+    public class CheeseAdaptee : IAdapt
+    {
+        public string Request() => "Cheese";
+    }
+
+    public class Target
+    {
+        public virtual string Request() => "Target";
+    }
+}
