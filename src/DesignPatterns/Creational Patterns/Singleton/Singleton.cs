d25b8dfb66194fd68e1d06904960f1ba777b5d2d
jmeline <jacob.meline@gmail.com>
2017-11-20 08:25:52 -0700

    Adding creational/singleton pattern

---
+++ src/DesignPatterns/Creational Patterns/Singleton/Singleton.cs
@@ -0,0 +1,15 @@
+﻿
+namespace DesignPatterns.Creational_Patterns.Singleton
+{
+    /// <summary>
+    /// Ensure a class has only one instance and provide a global point of access to it
+    /// 
+    /// Frequency of use: 4/5
+    /// </summary>
+    public class Singleton<T> where T : class, new()
+    {
+        private static T _instance; 
+
+        public static T Instance() => _instance ?? (_instance = new T());
+    }
+}
---
+++ src/DesignPatterns/Creational Patterns/Singleton/SingletonTests.cs
@@ -0,0 +1,22 @@
+﻿
+using Xunit;
+
+namespace DesignPatterns.Creational_Patterns.Singleton
+{
+    
+    public class SingletonTests
+    {
+        private class Test1 { }
+
+        [Fact]
+        public void Test1IsASingleton()
+        {
+            var s1 = Singleton<Test1>.Instance();
+            var s2 = Singleton<Test1>.Instance();
+            if (s1 != s2)
+            {
+                Assert.False(true);
+            }
+        }
+    }
+}
